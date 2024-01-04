d224038fd7b7c373287d77449cf236cea420a30c
Garrett HE <garrett.he@outlook.com>
2024-01-04 10:57:23 +0800

    Add extra information for programs

--- electron/dostron.ts
+++ electron/dostron.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import {Program} from "dostron/types";
+import {Program, ProgramInfo} from "dostron/types";
 
 export function discoverPrograms(dir: string): Program[] {
     return fs.readdirSync(dir, {withFileTypes: true})
@@ -11,6 +11,16 @@ export function discoverPrograms(dir: string): Program[] {
 function loadProgram(dir: string): Program {
     return <Program>{
         id: path.basename(dir),
-        dir
+        dir,
+        info: loadProgramInfo(dir)
     };
+}
+
+function loadProgramInfo(dir: string): ProgramInfo {
+    const filePath = path.resolve(getDataDirectoryPath(dir), "info.json");
+    return fs.existsSync(filePath) ? JSON.parse(fs.readFileSync(filePath, {encoding: "utf-8"})) : <ProgramInfo>{name: path.basename(dir)};
+}
+
+function getDataDirectoryPath(base: string): string {
+    return path.resolve(base, ".dostron");
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,5 +2,17 @@ declare module "dostron/types" {
     interface Program {
         id: string;
         dir: string;
+        info: ProgramInfo;
+    }
+
+    interface ProgramInfo {
+        name: string;
+        language?: string;
+        release?: number;
+        category?: string;
+        developer?: string;
+        publisher?: string;
+        series?: string;
+        tags?: string[];
     }
 }
